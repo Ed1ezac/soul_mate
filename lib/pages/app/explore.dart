e8dc892c27afb2a3b1a2af7fb0eb54a9497a94d2
Ed1ezac <ed1ezac@gmail.com>
2020-10-28 13:19:27 +0200

    added missing explore class

---
+++ lib/pages/app/explore.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Explore extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ExploreState();
+  }
+}
+
+class ExploreState extends State<Explore> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+    );
+  }
+}

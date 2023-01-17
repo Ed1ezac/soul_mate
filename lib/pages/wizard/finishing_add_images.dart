46ae9992c02e3bf47f49f657d0cf4af7b4c96645
Ed1ezac <ed1ezac@gmail.com>
2023-01-18 01:47:56 +0200

    adds finishing steps to wizard

---
+++ lib/pages/wizard/finishing_add_images.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddImages extends StatefulWidget {
+  const AddImages({Key? key}) : super(key: key);
+
+  @override
+  _AddImagesState createState() => _AddImagesState();
+}
+
+class _AddImagesState extends State<AddImages> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/wizard/finshing_ground_rules.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GroundRules extends StatelessWidget {
+  const GroundRules({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        //show rules
+        );
+  }
+}
